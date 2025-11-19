356585130425f8c1de927c6aca138f06eaed449c
444102466 <444102466@student.ksu.edu.sa>
2025-11-19 17:18:31 +0300

    V1.1

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import CourseNote
+from .forms import CourseNoteForm
+from django.contrib import messages
+
 
 # Create your views here.
+
+def index(request):
+   
+    notes = CourseNote.objects.all()
+    context = {
+        'notes': notes,
+        'total_notes': notes.count()
+    }
+    return render(request, 'notes/note_list.html', context)
